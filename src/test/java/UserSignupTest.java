42c51b5eda9685a7b220bc0bb876fdf8cdb038e4
AST-LW <walkthroughfrom2020@gmail.com>
2024-01-27 12:04:56 +0530

    Randomizing email and improved assertions

--- src/test/java/UserSignupTest.java
+++ src/test/java/UserSignupTest.java
@@ -5,6 +5,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.sql.SQLOutput;
+import java.util.UUID;
 
 public class UserSignupTest extends BaseAPITest {
     @Test
@@ -12,7 +13,7 @@ public class UserSignupTest extends BaseAPITest {
         // Arrange
         String signupEndpointResource = "/api/auth/signup";
 
-        String email = "jane11@ultralesson.com";
+        String email = UUID.randomUUID().toString() + "@ultralesson.com";
         String password = "1234567890";
 
         String signupRequestBody = String.format("{\"email\": \"%s\", \"password\": \"%s\"}", email, password);
@@ -29,9 +30,9 @@ public class UserSignupTest extends BaseAPITest {
         String accessToken = response.jsonPath().get("data.session.access_token");
 
         // Assert
-        Assert.assertEquals(statusCode, 201, "Failed due to wrong status code");
-        Assert.assertEquals(authenticatedEmail, email, "Failed due to incorrect email");
-        Assert.assertEquals(role, "authenticated", "Failed to validate the role to be authenticated");
-        Assert.assertNotNull(accessToken, "Failed as access token is null");
+        Assert.assertEquals(statusCode, 201, "The API did not return the expected 201 Created status code");
+        Assert.assertEquals(authenticatedEmail, email, "The returned email does not match the one used during signup");
+        Assert.assertEquals(role, "authenticated", "The user role is not set to 'authenticated'");
+        Assert.assertNotNull(accessToken, "The access token should not be null after successful signup.");
     }
 }
